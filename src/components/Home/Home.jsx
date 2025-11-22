cc631cf91924d389ae6b5dd8485e88ed532a14c1
owais aslam <waqqasqaziqazi@gmail.com>
2025-11-22 15:40:45 +0500

    new change

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1919,18 +1919,7 @@ const Home = () => {
                   <div className="col-left">
                     <a href="index.html" className="logo-custom">
                       <div className="logo-site-sv">
-                        <svg
-                          width={34}
-                          height={41}
-                          viewBox="0 0 34 41"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M9.21094 0.400391C15.9908 0.392629 21.8983 0.597548 27.1426 5.65918C35.1922 13.4293 35.2468 26.7035 27.5312 34.7549C22.0053 40.1145 16.6295 40.3989 9.48535 40.4004L0.75 40.3945L0.751953 0.407227L9.21094 0.400391ZM21.5244 6.83496C16.6613 3.86011 10.4273 4.54391 4.91113 4.55664L17.2607 22.3262C19.5017 25.5466 21.7744 28.9254 24.0615 32.0889C24.847 31.468 25.7087 30.3488 26.2646 29.5088C28.7144 25.5207 29.444 20.7119 28.2871 16.1777C27.305 12.2252 25.0035 8.9637 21.5244 6.83496Z"
-                            fill="#07C42C"
-                          />
-                        </svg>
+                        <img style={{width:65,height:50}} src="/assets/images/logo/logo.png" alt="" />
                       </div>
                       <span className="line-vertical left" />
                       <span className="line-vertical right" />
